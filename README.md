ef3b4212736fdb2fdae331ab6802c9212be39bb5
Alina Nanu <nanualinav@gmail.com>
2023-03-15 13:31:28 +0200

    create contact page, add final changes to the project.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # portfolio-website
+This is a **personal portfolio site**, a static website where I am sharing the projects that I worked on in my **CodeCademy** Path.
--- main.js
+++ main.js
@@ -12,3 +12,23 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+function MenuCtrl($) {
+    var self = this;
+
+    self.items = $('#menu li');
+    self.ACTIVE_CLASS = 'is-active';
+
+    self.items.click(function(event) {
+        event.preventDefault();
+        self.items
+            .removeClass(self.ACTIVE_CLASS)
+            .filter($(this))
+            .addClass(self.ACTIVE_CLASS);
+    });
+
+    self.items.filter('.is-default').click();
+    ('.nav-item a').addClass("active");
+}
+
+jQuery(document).ready(MenuCtrl);
